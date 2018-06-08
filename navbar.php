6ffbcdb8129f5aff510a1964811575712834800e
Ludovic Dhoore <Master@MacBook-Pro-de-Ludovic-3.local>
2018-06-09 01:18:57 +0200

    responsive

--- navbar.php
+++ navbar.php
@@ -10,6 +10,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
          <link rel="stylesheet" type="text/css" href="navbar.css" />
+         <link rel= "manifest" href= "manifest.json">
+          <script src="sw.js"></script>
     </head>
     <body>
           <header id="header_navbar">
